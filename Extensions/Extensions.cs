678203e2d25489e36d6a28060dd56c1844a32f21
Jared Prather <pratherjared@gmail.com>
2013-10-24 21:21:57 -0500

    Added bitbucket POST hook and commit model

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ForeverDeploy.Extensions
+{
+	public static class Extensions
+	{
+		//Returns a string x y ago from a datetime
+		public static string TimeDiffAgeSingleUnit(this DateTime startTime)
+		{
+			var age = (DateTime.UtcNow - startTime);
+			string ageString = "";
+			if (age.Days >= 1)
+			{
+				ageString = ((int)age.Days).ToString() + " days ago";
+			}
+			else
+				if (age.Hours >= 1)
+				{
+					ageString = ((int)age.Hours).ToString() + " hours ago";
+				}
+				else
+					if (age.Minutes >= 1)
+					{
+						ageString = ((int)age.Minutes).ToString() + " minutes ago";
+					}
+					else
+						if (age.Seconds >= 1)
+						{
+							ageString = ((int)age.Seconds).ToString() + " seconds ago";
+						}
+			return ageString;
+		}
+
+
+		public static string TimeDiffRemainSingleUnit(this TimeSpan age)
+		{
+			string ageString = "";
+			if (age.Days >= 1)
+			{
+				ageString = ((int)age.Days).ToString() + " days remaining";
+			}
+			else
+				if (age.Hours >= 1)
+				{
+					ageString = ((int)age.Hours).ToString() + " hours remaining";
+				}
+				else
+					if (age.Minutes >= 1)
+					{
+						ageString = ((int)age.Minutes).ToString() + " minutes remaining";
+					}
+					else
+						if (age.Seconds >= 1)
+						{
+							ageString = ((int)age.Seconds).ToString() + " seconds remaining";
+						}
+			return ageString;
+		}
+	}
+
+}
---
+++ Models/BitBucketCommit.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using ForeverDeploy.Extensions;
+
+namespace ArrowManager.Models
+{
+	[Table("Commits")]
+	public class Commit
+	{
+		[Key]
+		[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+
+		public string author { get; set; }
+		public string message { get; set; }
+		public string raw_node { get; set; }
+		public string raw_author { get; set; }
+		public List<File> files { get; set; }
+		public string branch { get; set; }
+		public string utctimestamp { get; set; }
+
+		public string CommitAge
+		{
+			get
+			{
+				return DateCommited.TimeDiffAgeSingleUnit();
+			}
+		}
+
+		public DateTime DateCommited
+		{
+			get
+			{
+				return (Convert.ToDateTime(utctimestamp)).AddHours(5);
+			}
+		}
+
+		[NotMapped]
+		public string timestamp { get; set; }
+		[NotMapped]
+		public int size { get; set; }
+		[NotMapped]
+		public string node
+		{
+			get
+			{
+				return raw_node.Substring(0, 7);
+			}
+		}
+		[NotMapped]
+		public List<string> parents { get; set; }
+		[NotMapped]
+		public object revision { get; set; }
+	}
+	[Table("Files")]
+	public class File
+	{
+		[Key]
+		[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+		public int ID { get; set; }
+		public int? CommitID { get; set; }
+		public virtual Commit Commit { get; set; }
+		public string type { get; set; }
+		public string file { get; set; }
+		public string fileName
+		{
+			get
+			{
+				return file.Substring(file.LastIndexOf('/') + 1, file.Length - file.LastIndexOf('/') - 1);
+			}
+		}
+	}
+
+	public class Repository
+	{
+		public string website { get; set; }
+		public bool fork { get; set; }
+		public string name { get; set; }
+		public string scm { get; set; }
+		public string owner { get; set; }
+		public string absolute_url { get; set; }
+		public string slug { get; set; }
+		public bool is_private { get; set; }
+	}
+
+	public class RootObject
+	{
+		public Repository repository { get; set; }
+		public bool truncated { get; set; }
+		public List<Commit> commits { get; set; }
+		public string canon_url { get; set; }
+		public string user { get; set; }
+	}
+}
